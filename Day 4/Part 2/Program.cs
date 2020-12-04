47bc262785a2c75567681f3711ff2083ab914e1a
Andrew Stobart <astobart@microsoft.com>
2020-12-04 15:14:22 -0600

    Day 4 part 2 work. Not yet finished.

--- Day 4/Part 2/Program.cs
+++ Day 4/Part 2/Program.cs
@@ -1,30 +1,20 @@
-﻿/* Advent of Code 2020
- * 
- * Programmer: Andrew Stobart
- * Date: December 4,2020
- * 
- * Day 4 Part 1
- * 
- * This code could be made much more efficient.... but it got me the write answer and I'm leaving it like this.
- */
-
-
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 
-namespace Part_1
+namespace Part_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2020\Day 4\Part 1\input.txt");
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2020\Day 4\Part 2\input_test.txt");
 
             string passport = "";
 
-            string[,] passportValidation = new string[2,8];
+            string[,] passportValidation = new string[2, 8];
 
             passportValidation[0, 0] = "byr";
             passportValidation[0, 1] = "iyr";
@@ -69,35 +59,35 @@ namespace Part_1
                     {
                         if (subStrings[theLoop] == "byr")
                         {
-                            passportValidation[1,0] = subStrings[theLoop + 1];
+                            passportValidation[1, 0] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "iyr")
                         {
-                            passportValidation[1,1] = subStrings[theLoop + 1];
+                            passportValidation[1, 1] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "eyr")
                         {
-                            passportValidation[1,2] = subStrings[theLoop + 1];
+                            passportValidation[1, 2] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "hgt")
                         {
-                            passportValidation[1,3] = subStrings[theLoop + 1];
+                            passportValidation[1, 3] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "hcl")
                         {
-                            passportValidation[1,4] = subStrings[theLoop + 1];
+                            passportValidation[1, 4] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "ecl")
                         {
-                            passportValidation[1,5] = subStrings[theLoop + 1];
+                            passportValidation[1, 5] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "pid")
                         {
-                            passportValidation[1,6] = subStrings[theLoop + 1];
+                            passportValidation[1, 6] = subStrings[theLoop + 1];
                         }
                         else if (subStrings[theLoop] == "cid")
                         {
-                            passportValidation[1,7] = subStrings[theLoop + 1];
+                            passportValidation[1, 7] = subStrings[theLoop + 1];
                         }
                     }
 
@@ -108,7 +98,7 @@ namespace Part_1
                     if (CheckValidity(passportValidation) == true)
                     {
                         numberOfValidPassports++;
-                        Console.WriteLine("Passport number " + totalNumberOfPassports + " is valid. Passport = " + passport + ". Total valid = " +numberOfValidPassports);
+                        Console.WriteLine("Passport number " + totalNumberOfPassports + " is valid. Passport = " + passport);
                     }
                     else
                     {
@@ -171,7 +161,7 @@ namespace Part_1
             if (CheckValidity(passportValidation) == true)
             {
                 numberOfValidPassports++;
-                Console.WriteLine("Passport number " + totalNumberOfPassports + " is valid. Passport = " + passport + ". Total valid = " + numberOfValidPassports);
+                Console.WriteLine("Passport number " + totalNumberOfPassports + " is valid. Passport = " + passport);
             }
             else
             {
@@ -186,22 +176,72 @@ namespace Part_1
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+
+            /*
+            That's not the right answer; your answer is too low. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, 
+            or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 181.) [Return to Day 4]
+            */
         }
+
 
         public static bool CheckValidity(string[,] pv)
         {
-            if (pv [1,0] == "" || pv[1,1] == "" || pv[1,2] == "" || pv[1,3] == "" || pv[1,4] == "" || pv[1,5] == "" || pv[1,6] == "")
+            if (pv[1, 0] == "" || pv[1, 1] == "" || pv[1, 2] == "" || pv[1, 3] == "" || pv[1, 4] == "" || pv[1, 5] == "" || pv[1, 6] == "")
             {
                 return false;
             }
             else
             {
-                return true;
+                bool stillValid = true;   //let's assume the best first
+                //landing here indicates that all required fields are present. Now we need to check if the data is valid or not
+
+                //checking Birth Year (byr) - cell 1,0
+                if (Convert.ToInt32(pv[1,0]) < 1920 || Convert.ToInt32(pv[1,0]) > 2002)
+                {
+                    stillValid = false;
+                }
+
+                //checking Issue Year (iyr) - cell 1,1
+                if (Convert.ToInt32(pv[1,1]) < 2010 || Convert.ToInt32(pv[1,1]) > 2020)
+                {
+                    stillValid = false;
+                }
+
+
+                //checking Expiration Year (eyr) - cell 1,2
+                if (Convert.ToInt32(pv[1, 2]) < 2020 || Convert.ToInt32(pv[1, 2]) > 2030)
+                {
+                    stillValid = false;
+                }
+
+
+                //checking Height (hgt) - cell 1,3
+                    int string resultString = Regex.Match(subjectString, @"\d+").Value;
+
+
+
+                //checking Hair Color (hcl - cell 1,4
+
+
+                //checking Eye Color (ecl) - cell 1,5
+                if (pv[1,5] == "amb" || pv[1, 5] == "blu" || pv[1, 5] == "brn" || pv[1, 5] == "gry" || pv[1, 5] == "hzl" || pv[1, 5] == "oth")
+                {
+                    stillValid = false;
+                }
+
+
+                //checking Passport ID - cell 1,6
+                if ((pv[1,6]).Length < 9 || pv[1,6].Length > 9)
+                {
+                    stillValid = false;
+                }
+
+
+
+
+                    return stillValid;
             }
-            
+
         }
-
-
     }
 }
-
