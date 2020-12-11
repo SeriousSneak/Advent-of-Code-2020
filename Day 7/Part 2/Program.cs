4fb1a44f13517d98510a1edbc0df2df6811a0969
Andrew Stobart <astobart@microsoft.com>
2020-12-11 14:02:33 -0600

    Start of Day 7 Part 2

--- Day 7/Part 2/Program.cs
+++ Day 7/Part 2/Program.cs
@@ -1,12 +1,14 @@
 ﻿/* Advent of Code 2020
  * 
  * Programmer: Andrew Stobart
- * Started December 8,2020
- * Solved December 11, 2020
+ * Started December 11,2020
+ * Solved
  * 
- * Day 7 Part 1
+ * Day 7 Part 2
  *
  */
+
+
 
 using System;
 using System.Collections.Generic;
@@ -15,20 +17,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Part_1
+namespace Part_2
 {
-
-    
-    
     class Program
     {
-
         static void Main(string[] args)
         {
             var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2020\Day 7\Part 1\input.txt");
 
-                        
-            string[] stringSeparators = new string[] { "contain" , " " , "bags" , "bag" , "." , "," , ""};
+
+            string[] stringSeparators = new string[] { "contain", " ", "bags", "bag", ".", ",", "" };
             Dictionary<string, List<string>> myDic = new Dictionary<string, List<string>>();
 
             foreach (var line in lines)
@@ -39,7 +37,7 @@ namespace Part_1
                 //set the key for the dictionary to be the first colour on the line
                 string[] currentLine = line.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
 
-                for (int x = 3; x < currentLine.Count(); x+=3)
+                for (int x = 3; x < currentLine.Count(); x += 3)
                 {
                     if (currentLine[x] != "" && currentLine[x] != "other")
                     {
@@ -54,12 +52,12 @@ namespace Part_1
                 outsideBag = currentLine[0] + " " + currentLine[1];
 
                 myDic.Add(outsideBag, insideBagsList);
-                
-                
+
+
             }
 
             int bagCounter = 0;
-            foreach(string item in myDic.Keys)
+            foreach (string item in myDic.Keys)
             {
                 if (item != "shiny gold")    //prevents us from couting if the outside bag is Shiny gold. We don't want Shiny gold to be the outside bag
                 {
@@ -70,7 +68,7 @@ namespace Part_1
                 }
             }
 
-            Console.WriteLine("There are " + bagCounter + " bags that can contain shiny gold bags."); 
+            Console.WriteLine("There are " + bagCounter + " bags that can contain shiny gold bags.");
 
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue.");
@@ -101,6 +99,5 @@ namespace Part_1
                 return false;
             }
         }
-        
     }
 }
